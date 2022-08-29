23007d371186c296b76942dc21d1af970952f610
cheezeomelette <dlehdcks156@naver.com>
2022-08-30 00:36:25 +0900

    08.30

---
+++ Level2/C#/CorrectBraket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodingTest_LeeDongChan
+{
+	public class CorrectBraket
+	{
+        public bool solution(string s)
+        {
+            bool answer = true;
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                    stack.Push(s[i]);
+                else if (s[i] == ')')
+                {
+                    if (stack.Count > 0)
+                        stack.Pop();
+                    else return false;
+                }
+
+            }
+            if (stack.Count > 0)
+                return false;
+            return answer;
+        }
+    }                                                                         
+}																				
