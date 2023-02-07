3f620c71cfc07e2d2e56466ce1bb5bd8e9357f28
cheezeomelette <dlehdcks156@naver.com>
2023-02-08 03:08:42 +0900

    2023.02.08 Solved

---
+++ C#/CreateStarNode.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest_LeeDongChan
+{
+	class CreateStarNode
+	{
+		/// <summary>
+		/// 1. n개 직선 배열에서 2개 조합
+		/// 2. 평행한지 확인 후 계속
+		/// 3. 정수인지 체크하기
+		/// 4. 직선의 교점 구하기
+		/// 
+		/// 5. 교점의 최대 x, y 최소 x, y 구하기
+		/// 6. 최대 최소를 바탕으로 배열 생성
+		/// 7. 교점이 있으면 * 없으면 .
+		/// </summary>
+
+		struct Cordinate
+		{
+			public int x;
+			public int y;
+			public Cordinate(int x, int y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+		}
+		public string[] solution(int[,] line)
+		{
+			string[] answer = new string[] { };
+			List<Cordinate> crossNode = new List<Cordinate>();
+			int minX = int.MaxValue;
+			int minY = int.MaxValue;
+			int maxX = int.MinValue;
+			int maxY = int.MinValue;
+
+			// 1. n개 직선 배열에서 2개 조합
+			for (int i = 0; i < line.GetLength(0) - 1; i++)
+			{
+				for (int j = i + 1; j < line.GetLength(0); j++)
+				{
+					// 2. 평행한지 확인 후 계속
+					int denominator = line[i, 0] * line[j, 1] - line[i, 1] * line[j, 0];
+					if (denominator == 0)
+						continue;
+					int numeratorA = line[i, 1] * line[j, 2] - line[i, 2] * line[j, 1];
+					int numeratorB = line[i, 2] * line[j, 0] - line[i, 0] * line[j, 2];
+
+					// 3. 정수인지 체크하기
+					if (numeratorA % denominator != 0 || numeratorB % denominator != 0)
+						continue;
+
+					// 4.직선의 교점 구하기
+					int x = numeratorA / denominator;
+					int y = numeratorB / denominator;
+					crossNode.Add(new Cordinate(x, y));
+
+					// 5. 교점의 최대 x, y 최소 x, y 구하기
+					minX = Math.Min(x, minX);
+					minY = Math.Min(y, minY);
+					maxX = Math.Max(x, maxX);
+					maxY = Math.Max(y, maxY);
+				}
+			}
+
+			// 6. 최대 최소를 바탕으로 배열 생성
+			List<string> plotList = new();
+			StringBuilder builder = new StringBuilder();
+			for (int y = maxY; y > minY - 1; y--)
+			{
+				for(int x = minX; x < maxX + 1; x++)
+				{
+					// 7. 교점이 있으면 * 없으면 .
+					char point = crossNode.Contains(new(x, y)) ? '*' : '.' ;
+					builder.Append(point);
+				}
+				plotList.Add(builder.ToString());
+				builder.Clear();
+			}
+
+			answer = plotList.ToArray();
+			return answer;
+		}
+
+
+	}
+}
---
+++ C#/SeesawPartner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest_LeeDongChan
+{
+	class SeesawPartner
+	{
+		/// <summary>
+		/// 1. 중복을 제거하기 위해 몸무게를 key 중복 개수를 value로 하는 dictionary 생성한다.
+		/// 2. key값을 선회하면서 key값의 4/3배, 3/2배, 2배 값이 dictionary에 있는지 확인하고 만약 있다면 각 weight의 value값을 곱해서 answer에 추가한다.
+		/// 3. 같은 weight의 사람끼리 선택하는 경우를 추가한다.
+		/// </summary>
+
+		public long solution(int[] weights)
+		{
+			long answer = 0;
+
+			// 1. 중복을 제거하기 위해 몸무게를 key 중복 개수를 value로 하는 dictionary 생성한다.
+			Dictionary<int, int> weightPeople = new Dictionary<int, int>();
+			foreach(int weight in weights)
+			{
+				if (weightPeople.ContainsKey(weight))
+					weightPeople[weight] += 1;
+				else
+					weightPeople[weight] = 1;
+			}
+
+			foreach(int weight in weightPeople.Keys)
+			{
+				long count = weightPeople[weight];
+				
+				// 2. key값을 선회하면서 key값의 4/3배, 3/2배, 2배 값이 dictionary에 있는지 확인한다.
+				if (weight % 3 == 0 && weightPeople.ContainsKey(weight * 4 / 3))
+					answer += count * weightPeople[weight * 4 / 3];
+				if(weight % 2 == 0 && weightPeople.ContainsKey(weight * 3 / 2))
+					answer += count * weightPeople[weight * 3 / 2];
+				if(weightPeople.ContainsKey(weight * 2))
+					answer += count * weightPeople[weight * 2];
+
+				// 3. 같은 weight의 사람끼리 선택하는 경우를 추가한다.
+				answer += count * (count - 1) / 2;
+			}
+			return answer;
+		}
+	}
+}
